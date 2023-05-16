8c0dd9be9e71f54b977a55f30fa6d170e5f17a71
Anton <anton.gap99@gmail.com>
2023-05-16 10:40:49 +0300

    some changes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,22 +2,33 @@ import { MouseEventHandler } from "react";
 import style from "./Button.module.css";
 import cn from "classnames";
 import { MouseEvent } from "react";
+import { apiHost } from "../../const/domain";
 
-
-export type ButtonType = "submit" | "button" | "reset" | undefined
+export type ButtonType = "submit" | "button" | "reset" | undefined;
 
 interface Props {
-  type: ButtonType,
-  text:string,
-  handle?: ()=> void
+  type: ButtonType;
+  text: string;
+  handle?: () => void;
 }
 
-export const Button = ({type, text, handle }: Props) => {
+const idInstance = 1101820960;
+const apiToken =  'a86af254e4b344708a9b75e7b4b71164133ea5aebc104a1a8b';
 
-  // const onClickHandle: MouseEventHandler  = (e) => {
-  //   handle && handle(e)
-  // }
+const receiveMessage = async () => {
+  try {
+    console.log(2);
+    const data = await fetch(`${apiHost}waInstance${idInstance}/receiveNotification/${apiToken}`);
+    const res = await data.json()
+    console.log('res: ', res);
 
+
+  } catch (error) {
+    console.log('error: ', error);
+  }
+}
+
+export const Button = ({ type, text }: Props) => {
   return (
     <button
       type={type}
@@ -25,6 +36,9 @@ export const Button = ({type, text, handle }: Props) => {
         primary: type === "submit",
         defualt: type !== "submit",
       })}
+      onClick={() => {
+        receiveMessage()
+      }}
     >
       {text}
     </button>
--- src/components/Chat/SendForm/SendForm.tsx
+++ src/components/Chat/SendForm/SendForm.tsx
@@ -4,7 +4,11 @@ import { FormEvent } from 'react';
 import { useAppDispatch } from '../../../redux/redux-hooks/hooks';
 import { messagesActions } from '../../../redux/messagesSlice/messagesReducer';
 import uniqid from 'uniqid';
+import { apiHost } from '../../../const/domain';
 
+
+const idInstance = 1101820960;
+const apiToken =  'a86af254e4b344708a9b75e7b4b71164133ea5aebc104a1a8b';
 
 export const SendForm = () => {
   const dispatch = useAppDispatch()
@@ -12,6 +16,21 @@ export const SendForm = () => {
     e.preventDefault();
     const target = e.target as HTMLFormElement;
     if(target.input.value === '') return;
+    fetch(`https://api.green-api.com/waInstance${idInstance}/SendMessage/${apiToken}`, {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "chatId": "79818797351@c.us",
+        "message": target.input.value
+    })
+    }).then((data) => {
+      console.log(data);
+      console.log('отправка');
+    }).catch((err) => {
+      console.log('не получилось');
+    })
     dispatch(messagesActions.addMessage({
       id: uniqid(),
       text: target.input.value,
@@ -21,12 +40,14 @@ export const SendForm = () => {
     console.log(target.input.value);
   }
 
+
+
   return (
     <div className = {style.wrapper}>
       <form className={style.form} onSubmit={onSubmitHandle}>
         <input name='input' className={style.sendInput} type="text" />
         <Button type='button' text='Обновить'/>
-        <Button type='submit' text='Отправить'/>
+        <Button type='submit' text='Отправить' />
       </form>
     </div>
   );
